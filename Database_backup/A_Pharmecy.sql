1b35f4e6e76b14d0d3f5a0aebbb114debf49a6ad
TANVIR <r3dr0k37@gmail.com>
2019-08-03 23:54:03 +0600

    db

--- Database_backup/A_Pharmecy.sql
+++ Database_backup/A_Pharmecy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 24, 2019 at 04:24 PM
+-- Generation Time: Aug 03, 2019 at 07:50 PM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.10
 
@@ -21,6 +21,29 @@ SET time_zone = "+00:00";
 --
 -- Database: `A_Pharmecy`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category_list`
+--
+
+CREATE TABLE `category_list` (
+  `id` int(11) NOT NULL,
+  `category` varchar(200) NOT NULL,
+  `description` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `category_list`
+--
+
+INSERT INTO `category_list` (`id`, `category`, `description`) VALUES
+(2, 'Pill', 'Bad\n'),
+(11, 'Injections', 'For Insuline'),
+(12, 'Injection', 'Needle'),
+(13, 'Syrup', 'asdfa'),
+(14, 'Dose', 'High Power');
 
 -- --------------------------------------------------------
 
@@ -47,12 +70,18 @@ CREATE TABLE `medicine_list` (
 --
 
 INSERT INTO `medicine_list` (`id`, `medicine_name`, `category`, `purchase_price`, `selling_price`, `store_box`, `quantity`, `genric_name`, `company`, `effects`, `expire_date`) VALUES
-(1, 'Ace', 'tablet', 15, 20, 'Paracetamol', 15, 'Paracetamol', 'Square', 'Fever', '2021-10-19'),
+(1, 'Ace', 'capsule', 32, 25, 'Paracetamol', 45, 'Paracetamol', 'Square', 'Fever', '2021-10-19'),
 (2, 'Aciflix', 'capsule', 20, 35, 'Esmoprazol', 50, 'Esmoprazol', 'Incepta', 'Gastrik', '2021-11-27');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `category_list`
+--
+ALTER TABLE `category_list`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `medicine_list`
@@ -63,6 +92,12 @@ ALTER TABLE `medicine_list`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `category_list`
+--
+ALTER TABLE `category_list`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `medicine_list`
